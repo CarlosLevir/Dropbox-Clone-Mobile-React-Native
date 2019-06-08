c65d362894754118ed695576c28517de67966ecb
Carlos Levir <carloslevirfs@gmail.com>
2019-06-08 15:37:06 -0300

    Initial commit.

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { StyleSheet, View } from "react-native";
+
+export default function App() {
+  return <View style={styles.container} />;
+}
+
+const styles = StyleSheet.create({
+  container: {}
+});
