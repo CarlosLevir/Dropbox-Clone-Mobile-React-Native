b691643b68679ab12583dfbd99578cfcebd25a4c
Carlos Levir <carloslevirfs@gmail.com>
2019-06-12 18:24:40 -0300

    Project Finalization.

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
-import React from "react";
-import { StyleSheet, View } from "react-native";
+import React from 'react';
+import { YellowBox } from 'react-native';
+
+import Routes from './routes';
+
+YellowBox.ignoreWarnings(['Unrecognized WebSocket']);
 
 export default function App() {
-  return <View style={styles.container} />;
+  return <Routes />;
 }
-
-const styles = StyleSheet.create({
-  container: {}
-});
